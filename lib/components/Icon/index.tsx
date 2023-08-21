1a85ef63203ca16192b37ddb2db545548faadf85
ninhtq97 <tranquangninh97@gmail.com>
2023-08-21 11:37:36 +0700

    Fix attr DOM

--- lib/components/Icon/index.tsx
+++ lib/components/Icon/index.tsx
@@ -35,8 +35,8 @@ export const ChevronDown: React.FC<IconProps> = (props) => {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6.58976 7.08976C6.26434 7.41517 5.73667 7.41517 5.41126 7.08976L0.697206 2.37567C0.371765 2.05027 0.371765 1.52264 0.697206 1.1972C1.02264 0.871767 1.55028 0.871767 1.87571 1.1972L6.00051 5.32201L10.1253 1.1972C10.4508 0.871767 10.9783 0.871767 11.3038 1.1972C11.6293 1.52264 11.6293 2.05027 11.3038 2.37567L6.58976 7.08976Z"
         fill="currentColor"
       />
@@ -90,20 +90,20 @@ export const Search: React.FC<IconProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_901_44154)">
+      <g clipPath="url(#clip0_901_44154)">
         <path
           d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18Z"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M20 20L16 16"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -125,27 +125,27 @@ export const ArrowLeft: React.FC<IconProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_908_30405)">
+      <g clipPath="url(#clip0_908_30405)">
         <path
           d="M5 12H19"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M5 12L9 16"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M5 12L9 8"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -167,27 +167,27 @@ export const ArrowRight: React.FC<IconProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_908_8440)">
+      <g clipPath="url(#clip0_908_8440)">
         <path
           d="M5 12H19"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15 16L19 12"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15 8L19 12"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -212,9 +212,9 @@ export const Check: React.FC<IconProps> = (props) => {
       <path
         d="M1.16663 3.50022L3.05551 5.38911L6.83329 1.61133"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -230,20 +230,20 @@ export const Plus: React.FC<IconProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_918_1476)">
+      <g clipPath="url(#clip0_918_1476)">
         <path
           d="M12 6.03516V18.0352"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M6 12.0352H18"
           stroke="currentColor"
-          stroke-width="1.75"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -270,27 +270,27 @@ export const EyeOff: React.FC<IconProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_18_1834)">
+      <g clipPath="url(#clip0_18_1834)">
         <path
           d="M3 3L21 21"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.584 10.5879C10.2087 10.9629 9.99775 11.4716 9.99756 12.0022C9.99737 12.5327 10.2079 13.0416 10.583 13.4169C10.958 13.7922 11.4667 14.0031 11.9973 14.0033C12.5278 14.0035 13.0367 13.7929 13.412 13.4179"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M17.357 17.3491C15.726 18.4491 13.942 19.0001 12 19.0001C8 19.0001 4.667 16.6671 2 12.0001C3.369 9.60506 4.913 7.82506 6.632 6.65906M9.363 5.36506C10.2204 5.11978 11.1082 4.9969 12 5.00006C16 5.00006 19.333 7.33306 22 12.0001C21.222 13.3611 20.388 14.5241 19.497 15.4881L9.363 5.36506Z"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -312,20 +312,20 @@ export const Eye: React.FC<IconProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_908_7007)">
+      <g clipPath="url(#clip0_908_7007)">
         <path
           d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M22 12C19.333 16.667 16 19 12 19C8 19 4.667 16.667 2 12C4.667 7.333 8 5 12 5C16 5 19.333 7.333 22 12Z"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
