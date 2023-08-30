3beaf165a5276339897d91edafab22f05aa45c55
ninhtq97 <tranquangninh97@gmail.com>
2023-08-30 16:44:04 +0700

    Add components InfiniteScroll

---
+++ lib/components/InfiniteScroll/index.tsx
@@ -0,0 +1,43 @@
+import { Icon, Spinner } from 'components/Icon';
+import { PropsWithChildren, forwardRef, useCallback, useRef } from 'react';
+
+type Props = {
+  isLoading: boolean;
+  hasMore: boolean;
+  onNext: () => void;
+  loader?: React.ReactNode;
+} & PropsWithChildren;
+
+const InfiniteScroll = forwardRef<HTMLElement, Props>(
+  ({ isLoading, hasMore, onNext, loader, children }) => {
+    const observer = useRef<IntersectionObserver>();
+    const $ref = useCallback(
+      (node: HTMLAnchorElement) => {
+        if (isLoading) return;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting && hasMore) {
+            onNext?.();
+          }
+        });
+        if (node) observer.current.observe(node);
+      },
+      [hasMore, isLoading],
+    );
+
+    return (
+      <>
+        {children}
+        {hasMore && (
+          <Icon
+            ref={$ref}
+            className="infinite-loading justify-center"
+            icon={hasMore && (loader || <Spinner className="animate-spin" />)}
+          />
+        )}
+      </>
+    );
+  },
+);
+
+export default InfiniteScroll;
