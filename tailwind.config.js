d45e836b628dff8147c9ff03a461742905e5be32
ninhtq97 <tranquangninh97@gmail.com>
2023-09-07 21:42:37 +0700

    Fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: ['class', '[data-mode="dark"]'],
   content: ['./lib/**/*.{js,jsx,ts,tsx}'],
   theme: {},
   plugins: [
