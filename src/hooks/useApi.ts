604bb62741375b08c05a5352ceb345d96698e6a4
ninhtq97 <tranquangninh97@gmail.com>
2023-11-11 12:06:01 +0700

    [Fix] Hook api, meta

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,23 +1,36 @@
 import { useCallback, useState } from 'react';
+import { getErrorMessage, removeStored } from 'utils';
+import useToast from './useToast';
 
-const useApi = () => {
-  const [isLoading, setIsLoading] = useState(false);
+const useApi = (redirectUrl: string, storeKey: string) => {
+  // const navigate = useNavigate();
+  const { onFire } = useToast();
+
+  const [loading, setLoading] = useState(false);
 
   const onCallWithCatchError = useCallback(
-    async <T extends Function>(callFn: T): Promise<T> => {
+    async <T extends Function>(callFn: T): Promise<T | null> => {
       try {
-        setIsLoading(true);
+        setLoading(true);
         return await callFn();
-      } catch (error) {
-        throw error;
+      } catch (error: any) {
+        let message = getErrorMessage(error);
+        onFire('error', message);
+
+        if (error.statusCode === 401 || error.message === 'Unauthorized') {
+          // navigate(redirectUrl);
+          removeStored(storeKey);
+        }
       } finally {
-        setIsLoading(false);
+        setLoading(false);
       }
+
+      return null;
     },
     [],
   );
 
-  return { isLoading, onCallWithCatchError };
+  return { onCallWithCatchError, loading };
 };
 
 export default useApi;
--- src/hooks/useMeta.ts
+++ src/hooks/useMeta.ts
@@ -1,10 +1,18 @@
 import { INIT_META } from 'constants/state';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { TMeta } from 'types';
+import { excludeEmptyValue } from 'utils';
 
-const useMeta = () => {
-  const [meta, setMeta] = useState(INIT_META);
+const useMeta = (state?: Partial<TMeta>) => {
+  const [meta, setMeta] = useState<TMeta>(
+    excludeEmptyValue({ ...INIT_META, ...state }),
+  );
 
-  return { meta, setMeta };
+  const onChangeMeta = useCallback((data: Partial<TMeta>) => {
+    setMeta((prev) => excludeEmptyValue({ ...prev, ...data }));
+  }, []);
+
+  return { meta, onChangeMeta };
 };
 
 export default useMeta;
